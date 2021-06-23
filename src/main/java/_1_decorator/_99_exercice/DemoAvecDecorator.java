862c5daad37747ce5886b99acce105a8ce02b480
alexandrepetrillo <alexandre.petrillo@gmail.com>
2021-06-23 11:21:59 +0200

    decorator

--- src/main/java/_1_decorator/_99_exercice/DemoAvecDecorator.java
+++ src/main/java/_1_decorator/_99_exercice/DemoAvecDecorator.java
@@ -1,17 +1,13 @@
 package _1_decorator._99_exercice;
 
 
-public class Demo {
+public class DemoAvecDecorator {
     public static void main(String[] args) {
         // exemple de données à sauvegarder encrypter et compressé
         String salaryRecords = "Name,Salary\nJohn Smith,100000\nSteven Jobs,912000";
 
         FileDataSource fileDataSource = new FileDataSource("OutputDemo.txt");
 
-        Encryptor encryptor = new Encryptor();
-        Compressor compressor = new Compressor();
-        String encrypted = encryptor.encode(salaryRecords);
-        String encryptedAndCompressed = compressor.compress(encrypted);
-        fileDataSource.writeData(encryptedAndCompressed);
+        // TODO
     }
 }
